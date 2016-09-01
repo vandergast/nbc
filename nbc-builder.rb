d298408ea3b5d5af7c0293220682acd63f806b47
vandergast <vandergst@gmail.com>
2016-09-01 14:25:26 -0400

    Fix: service folder was not being created from the app

--- nbc-builder.rb
+++ nbc-builder.rb
@@ -46,6 +46,8 @@ class NBC
 
 	private
 	def make_archive
+		puts "Creating services foler"
+		system("mkdir services")
 		puts "Creating HTML files..."
 		File.new("./services/digg-news.html", "w+")
 		File.new("./services/reddit-news.html", "w+")
@@ -104,15 +106,19 @@ class NBC
     	file.puts "<!DOCTYPE html>"
       file.puts "<html>"
       file.puts "<head>"
-      file.puts "<title>NBC NEWS</title>"
+      file.puts "\t<title>NBC NEWS</title>"
+      file.puts '\t<meta charset="utf-8">'
+      file.puts '\t<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">'
+			file.puts '\t<link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.indigo-pink.min.css">'
+			file.puts '\t<script defer src="https://code.getmdl.io/1.2.0/material.min.js"></script>'
       file.puts "<body>"
-      file.puts "\n\t<a href=./digg-news.html>DIGG NEWS</a> - <a href=./reddit-news.html>REDDIT NEWS</a> - <a href=./mashable-news.html>MASHABLE NEWS</a>"
+      file.puts '\t<a href="./digg-news.html">DIGG NEWS</a> - <a href=."/reddit-news.html">REDDIT NEWS</a> - <a href="./mashable-news.html">MASHABLE NEWS</a>'
       binder.each do |story|
-        file.puts "\n\t<p><b>Title:</b> #{story[:title]}<br>"
-        file.puts "\n\t<b>Author:</b> #{story[:author]}<br>"
-        file.puts "\n\t<b>Date:</b> #{(((Time.parse((story[:datetime]).to_s)).to_s).split(" "))[0]}<br>"
-        file.puts "\n\t<b>Time:</b> #{(((Time.parse((story[:datetime]).to_s)).to_s).split(" "))[1]}<br>"
-        file.puts "\n\t<b>Website:</b> <a href=#{story[:url]}>link</a><br>"
+        file.puts "\t<p><b>Title:</b> #{story[:title]}<br>"
+        file.puts "\t<b>Author:</b> #{story[:author]}<br>"
+        file.puts "\t<b>Date:</b> #{(((Time.parse((story[:datetime]).to_s)).to_s).split(" "))[0]}<br>"
+        file.puts "\t<b>Time:</b> #{(((Time.parse((story[:datetime]).to_s)).to_s).split(" "))[1]}<br>"
+        file.puts "\t<b>Website:</b> <a href=#{story[:url]}>link</a><br>"
         file.puts "<hr>"  
       end
       file.puts "</body>"
