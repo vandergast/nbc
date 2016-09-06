a54b9cf9793c1c93ca806a8c1a768d2cfe3518b3
Daniel <vandergst@gmail.com>
2016-09-06 08:56:53 -0400

    final project

--- classes/mashable.rb
+++ classes/mashable.rb
@@ -10,13 +10,35 @@ class Mashable
 	end
 	def get
     options = ["new","rising","hot"]
-    choice = options[rand(3)]
+    #choice = options[rand(3)]
     mashableuri = URI("http://mashable.com/stories.json")
     mashable = JSON.parse(Net::HTTP.get(mashableuri))
     count = 0
 	  art_num = 35
 	  binder = []
-	  mashable[choice].each do |stories|
+	  mashable[options[0]].each do |stories|
+	    binder[count] = {}
+      binder[count][:title] = stories["title"]
+      binder[count][:author] = stories["author"]
+      binder[count][:datetime] = (Time.parse(stories["post_date_rfc"])).to_datetime
+      binder[count][:url] = stories["link"]
+      binder[count][:content] = stories["content"]["plain"]
+      binder[count][:art_num] = art_num + 1
+	    count += 1
+	    art_num += 1
+	  end
+	  mashable[options[1]].each do |stories|
+	    binder[count] = {}
+      binder[count][:title] = stories["title"]
+      binder[count][:author] = stories["author"]
+      binder[count][:datetime] = (Time.parse(stories["post_date_rfc"])).to_datetime
+      binder[count][:url] = stories["link"]
+      binder[count][:content] = stories["content"]["plain"]
+      binder[count][:art_num] = art_num + 1
+	    count += 1
+	    art_num += 1
+	  end
+	  mashable[options[2]].each do |stories|
 	    binder[count] = {}
       binder[count][:title] = stories["title"]
       binder[count][:author] = stories["author"]
@@ -29,7 +51,6 @@ class Mashable
 	  end
 	  dp "Mashable news saved to service"
 	  return binder
-		rescue
-			ep "Trouble connecting to Mashable"
+
 	end
 end
